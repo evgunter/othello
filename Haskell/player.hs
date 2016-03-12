7d2188d666ce82b2bac88bbca5c5c7069793316f
Evan Gunter <evgunter@gmail.com>
2016-03-11 16:14:53 -0800

    update, now successfully plays several legal moves before error with taking head of empty list

--- Haskell/player.hs
+++ Haskell/player.hs
@@ -1,16 +1,19 @@
 import Data.List (mapAccumL)
 import GHC.Word (Word64)
 import Data.Bits --many things shall hail from here
-import System.Environment (getArgs)
+import System.Environment (getArgs, withArgs)
+import System.IO (hSetBuffering, BufferMode (LineBuffering), stdout)
 
-data OpponentsMove = OpponentsMove Int Int
-data MyMove        = MyMove Int Int
+data Move = Move Int Int
+type OpponentsMove = Move
+type MyMove        = Move
 data Board         = Board { filled :: !Word64, mine :: !Word64 }
                    deriving (Eq, Show, Ord)
 data Time          = Time Int
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
   [color] <- getArgs
   let (board, initialOutput)
         | color == "Black" = (boardWFirstMove, formatMyMove firstMove ++ "\n")
@@ -19,19 +22,28 @@ main = do
 
 -- applies opponents' and my moves
 oneMove :: Board -> OpponentsMove -> (Board, MyMove)
-oneMove b m = (Board (setBit (filled newb) (processMyMove mym)) (setBit (mine newb) (processMyMove mym)), mym)
-  where newb = Board (setBit (filled newb) (processOpponentsMove m)) (mine b)
-        mym = intToMyMove (head (validMoves newb))
+oneMove b m = (Board (setBit (filled newb) (processMove mym))
+                     (foldr (flip setBit) (setBit (mine newb) (processMove mym))
+                          toFlip),
+               mym)
+  where
+    newb = Board (setBit (filled b) (processMove m))
+                 (foldr (flip clearBit) (mine b) (flipped b m))
+    (mym, toFlip) = head' (validMoves newb)
+    head' [] = (Move (-1) (-1), [])
+    head' xs = (\(a, b) -> (intToMyMove a, b))  (head xs)
+
+--oneMove = pass where pass b  _ = (b, Move (-1) (-1))
 
 parseOneMove :: String -> (OpponentsMove, Time)
-parseOneMove = (\[l1, l2, l3] -> ((OpponentsMove l1 l2), Time l3)) .
+parseOneMove = (\[l1, l2, l3] -> ((Move l1 l2), Time l3)) .
                  ((map read) . words)
 
 intToMyMove :: Int -> MyMove
-intToMyMove n = MyMove (n `mod` 8) (n `div` 8)
+intToMyMove n = Move (n `mod` 8) (n `div` 8)
 
 formatMyMove :: MyMove -> String
-formatMyMove (MyMove x y) = (show x) ++ " " ++ (show y)
+formatMyMove (Move x y) = (show x) ++ " " ++ (show y)
 
 play :: Board -> String -> String
 play initBoard rawInput = unlines $ map formatMyMove myMoves
@@ -49,29 +61,40 @@ boardWFirstMove = Board
   (foldr (flip setBit) (0 :: Word64) (map (\(a, b) -> a + 8*b) [(3, 2), (3, 4)]))
   
 firstMove :: MyMove
-firstMove = MyMove 3 2
+firstMove = Move 3 2
 
-processOpponentsMove :: OpponentsMove -> Int
-processOpponentsMove (OpponentsMove x y) = 8*y + x
+processMove :: Move -> Int
+processMove (Move x y) = 8*y + x
 
-processMyMove :: MyMove -> Int
-processMyMove (MyMove x y) = 8*y + x
+notDone = error . ("NotDone: " ++)
 
-notDone :: a
-notDone = undefined
-
-validMoves :: Board -> [Int]
-validMoves b = filter (\k -> not (testBit (filled b) k)) $ concat $ map
-  (\j -> map (head . ((dropWhile (\i -> not (testBit (mine b) i) &&
-                                        (testBit (filled b) i))))) $
-             filter (\xs -> (testBit (filled b) (head xs))) $
+validMoves :: Board -> [(Int, [Int])]
+validMoves b = filter (\(k, _) -> not (testBit (filled b) k)) $ concat $ map
+  (\j -> map (takeWhileAndAfter (\i -> not (testBit (mine b) i) &&
+                                        (testBit (filled b) i))) $
+             filter (\xs -> (xs /= []) && (testBit (filled b) (head xs))) $
                     map ((takeWhile (\x -> (0 <= x) && (x <= 63))) . tail .
                          (\f -> iterate f j))
                         [(+ (-9)), (+ (-8)), (+ (-7)), (+ (-1)), (+ 1), (+ 7),
-                         (+ 8), (+ 9)])
-  myPieceIndexes
+                         (+ 8), (+ 9)]) $
+  filter (testBit (mine b)) [0..63]
+
+--list of spaces to be flipped from mine to opponents'
+flipped :: Board -> OpponentsMove -> [Int]
+flipped b mov = concat $ map (\(_, xs) -> xs) $ filter
+  (\(k, _) -> ((testBit (filled b) k) && (not (testBit (mine b) k)))) $
+  map (takeWhileAndAfter (\i -> (testBit (mine b) i))) $
+        filter (\xs -> (xs /= []) && (testBit (filled b) (head xs))) $
+               map ((takeWhile (\x -> (0 <= x) && (x <= 63))) . tail .
+                    (\f -> iterate f m))
+                   [(+ (-9)), (+ (-8)), (+ (-7)), (+ (-1)), (+ 1), (+ 7),
+                    (+ 8), (+ 9)]
   where
-    myPieceIndexes = filter (testBit (mine b)) [0..63]
+    m = processMove mov
+
+takeWhileAndAfter :: (a -> Bool) -> [a] -> (a, [a])
+takeWhileAndAfter f xs = (head b, a)
+  where (a, b) = span f xs
 
 -- adjacentSquares :: Int -> [Int]
 -- adjacentSquares n = [n - 9, n - 8, n - 7, n - 1, n + 1, n + 7, n + 8, n + 9]
