9209636cc2767bba0d7a1a83f15ebc79e420b61c
Evan Gunter <evgunter@gmail.com>
2016-03-13 23:50:10 -0700

    neatness, fixed some minor bugs, maybe broke some stuff

--- Player.hs
+++ Player.hs
@@ -1,7 +1,7 @@
 import Debug.Trace (trace)
-import Data.List (mapAccumL, maximumBy, minimumBy, sort, groupBy)
+import Data.List (mapAccumL, maximumBy, sort, groupBy)
 import GHC.Word (Word64)
-import Data.Bits --many things shall hail from here
+import Data.Bits (Bits, testBit, setBit, bitSize, clearBit, complement, (.&.))
 import System.Environment (getArgs, withArgs)
 import System.IO (hSetBuffering, BufferMode (LineBuffering), stdout)
 import Data.Maybe (catMaybes)
@@ -12,14 +12,13 @@ type OpponentsMove = Move
 type MyMove        = Move
 type BoardPosIndex = Int
 data Board         = Board { filled :: !Word64, mine :: !Word64 }
-                   deriving (Eq, Ord)
-data Time          = Time Int
-data Tree a        = Branches [Tree a] | Leaf a deriving (Eq, Ord, Show)
+                   deriving (Eq)
+type Time          = Int
 
 instance Show Board where
- show (Board filled mine) = unlines $ groupsOf 8
-   $ map onePos [0..bitSize filled - 1]
-   where onePos i = case (testBit filled i, testBit mine i) of
+ show (Board fill my) = unlines $ groupsOf 8
+   $ map onePos [0..bitSize fill - 1]
+   where onePos i = case (testBit fill i, testBit my i) of
                      (False, _)     -> '.'
                      (True,  False) -> 'O'
                      (True,  True)  -> 'M'
@@ -34,20 +33,12 @@ main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   [color] <- getArgs
-  {-
-  let (board, initialOutput)
-        | color == "Black" = (boardWFirstMove, formatMyMove firstMove ++ "\n")
-        | otherwise        = (initialBoard, "")
-  interact $ (("Sam is initialized.\n" ++ initialOutput) ++) .
-    playString board
-  -}
   let initBoard | color == "Black" = initialBBoard
                 | otherwise        = initialWBoard
   interact $ ("Sam is initialized.\n" ++ ) . playString initBoard
 
 -- applies opponents' and my moves
 oneMove :: Board -> OpponentsMove -> (Board, MyMove)
---oneMove b m = (Board (setBit (filled newb) (processMove mym))
 oneMove b m = trace ("oneMove: opponent: " ++ show m)
                     (newerBoard, trace traceString mym)
   where
@@ -75,30 +66,6 @@ currentStrategy :: [(BoardPosIndex,          [BoardPosIndex])] -> Board ->
                     (BoardPosIndex, [BoardPosIndex])
 currentStrategy = minimaxWrap
 
-simple :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
-           (BoardPosIndex, [BoardPosIndex])
-simple xs _ = head xs
-
-greedy :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
-           (BoardPosIndex, [BoardPosIndex])
-greedy xs _ = maximumBy best xs
-  where best (a, as) (b, bs) | length as >  length bs = GT
-                             | length as == length bs = EQ
-                             | otherwise              = LT
-
-greedyCorners :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
-                  (BoardPosIndex, [BoardPosIndex])
-greedyCorners xs _ = maximumBy best xs
-  where best (a, as) (b, bs) | a `elem` corners       = GT
-                             | b `elem` corners       = LT
-                             | a `elem` cornerDiag    = LT
-                             | b `elem` cornerDiag    = GT
-                             | a `elem` cornerEdge    = LT
-                             | b `elem` cornerEdge    = GT
-                             | length as >  length bs = GT
-                             | otherwise              = LT
-                             --no EQ because unnecessary
-
 minimaxWrap :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
                   (BoardPosIndex, [BoardPosIndex])
 minimaxWrap _ b = (\(a, as) -> (moveIndex a, as)) $ fst $ minimax b depth
@@ -120,21 +87,15 @@ emptyToPass :: [(Move, [BoardPosIndex])] -> [(Move, [BoardPosIndex])]
 emptyToPass [] = [(Move (-1) (-1), [])]
 emptyToPass x  = x
 
-{-
-games :: Board -> Tree
-games b | validMoves b == [] && validMoves (flipBoard b) == [] = b
-        | -}
-
-
 -- this heuristic function is based on https://kartikkukreja.wordpress.com/2013/03/30/heuristic-function-for-reversiothello/
 -- in particular, the relative weightings are copied directly.
 eval :: Board -> Double
-eval b@(Board f m) = 10 * totalTiles + 801.724 * corners +
-                     382.026 * cornerNext
+eval b@(Board f m) = 10 * totalTiles + 801.724 * cornersOcc +
+                     382.026 * cornerNext + 78.922 * mobility
   where
   totalTiles = 100 * (fromIntegral (2*(popCount' m) - (popCount' f)))
                    / (fromIntegral (popCount' f))
-  corners    =  fromIntegral $
+  cornersOcc =  fromIntegral $
       25 * (2 *  sum (map true1False0
                           [(testBit m  0       ), (testBit m  7      ),
                            (testBit m (8*7)    ), (testBit m (8*7 + 7))])
@@ -174,31 +135,16 @@ eval b@(Board f m) = 10 * totalTiles + 801.724 * corners +
   mobility = 100 * (lm - lo) / (lm + lo) where
     lm = fromIntegral $ length $ validMoves $           b
     lo = fromIntegral $ length $ validMoves $ flipBoard b
-  true1False0 b = if b then 1 else 0
+  true1False0 bool = if bool then 1 else 0
 
 depth :: Int
 depth = 5
-
-corners :: [BoardPosIndex]
-corners = map tupleToBoardIndex [(0, 0), (0, 7), (7, 0), (7, 7)]
-
-cornerDiag :: [BoardPosIndex]
-cornerDiag = map tupleToBoardIndex [(1, 1), (1, 6), (6, 1), (6, 6)]
-
-cornerEdge :: [BoardPosIndex]
-cornerEdge = map tupleToBoardIndex [(0, 1), (1, 0), (0, 6), (6, 0), (1, 7),
-                                    (7, 1), (6, 7), (7, 6)]
-
-edges :: [BoardPosIndex]
-edges = map tupleToBoardIndex $ concat $ map
-   (\n -> [(0, n), (7, n), (n, 0), (n, 7)]) [0..7]
 
 tupleToBoardIndex :: (Int, Int) -> BoardPosIndex
 tupleToBoardIndex (x, y) = 8*y + x
 
 parseOneMove :: String -> (OpponentsMove, Time)
-parseOneMove = (\[l1, l2, l3] -> ((Move l1 l2), Time l3)) .
---                 ((map read) . words)
+parseOneMove = (\[l1, l2, l3] -> ((Move l1 l2), l3)) .
                 ((map read) . words) . (\x -> trace ("Sam got: " ++ show x) x)
 
 boardPosToMyMove :: BoardPosIndex -> MyMove
@@ -214,13 +160,6 @@ playString initBoard rawInput = unlines $ map formatMyMove myMoves
 
 play :: Board -> [OpponentsMove] -> (Board, [MyMove])
 play = mapAccumL oneMove
-
-explicitPlay :: Board -> [OpponentsMove] -> (Board, [(Board, MyMove)])
-explicitPlay = explicitMapAccumL oneMove
-
-explicitMapAccumL :: (acc -> x -> (acc, y)) -> acc -> [x] -> (acc, [(acc, y)])
-explicitMapAccumL f = mapAccumL f' where
-  f' acc x = let (acc', y) = f acc x in (acc', (acc, y))
 
 initialWBoard :: Board -- Board with starting configuration for white
 initialWBoard = Board
@@ -239,21 +178,6 @@ initialBBoard = Board
 flipBoard :: Board -> Board
 flipBoard b = Board fill ((complement (mine b)) .&. fill) where
   fill = filled b
-{-
-boardWFirstMove :: Board -- Board with first move (3, 2)
-boardWFirstMove = Board
-  (foldr (flip setBit) (0 :: Word64)
-         (map (\(a, b) -> a + 8*b) [(3, 2), (3, 3), (4, 3), (3, 4), (4, 4)]))
-  (foldr (flip setBit) (0 :: Word64)
-         (map (\(a, b) -> a + 8*b) [(3, 2), (3, 4)]))
--}
-{-
-firstMove :: MyMove
-firstMove = Move 3 2
--}
-
---processMove :: Move -> BoardPosIndex
---processMove (Move x y) = 8*y + x
 
 checkValid :: Move -> Move
 checkValid m@(Move x y) | 0 <= x && x < 8 && 0 <= y && y < 8 = m
@@ -265,8 +189,6 @@ setMoveBit w m = setBit w $ moveIndex $ checkValid m
 
 moveIndex :: Move -> BoardPosIndex
 moveIndex (Move x y) = (8*y + x)
-
--- notDone = error . ("NotDone: " ++)
 
 validMoves :: Board -> [(BoardPosIndex, [BoardPosIndex])]
 validMoves b = map (foldr (\(a, as) (_, bs) -> (a, as ++ bs)) (-1, [])) $
@@ -307,84 +229,44 @@ popCount' :: Bits a => a -> Int
 popCount' x = length $ filter id $ map (testBit x) [0 .. (bitSize x) - 1]
 
 {-
-test :: (Board, [MyMove])
-test = play boardWFirstMove [Move 2 2, Move 2 4,
-                             Move 5 4, Move 4 2,
-                             Move 7 6, Move 4 5]
--}
-{-
-test = putStr $ unlines $ map show incr ++ [show final]
-  where (final, incr) = explicitPlay boardWFirstMove [Move 2 2, Move 2 4,
-                                                      Move 5 4, Move 4 2,
-                                                      Move 7 6, Move 4 5]
--}
-
+testdiagonal :: Board
 testdiagonal = Board (35978936582144 + 2^(4+5*8)) 35390664998912
 
--- adjacentSquares :: Int -> [Int]
--- adjacentSquares n = [n - 9, n - 8, n - 7, n - 1, n + 1, n + 7, n + 8, n + 9]
+simple :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
+           (BoardPosIndex, [BoardPosIndex])
+simple xs _ = head xs
 
-{-
-game with BetterPlayer
-3 2
-2 2 2
-1 2
-2 4 4
-5 5
-5 4 4
-6 5
-4 2 2
-3 5
-0 2 2
-1 1
-0 0 0
-1 4
-0 4 4
-2 1
-3 0 0
-1 0
-2 0 0
-5 2
-7 6 6
-1 5
-6 2 2
-3 1
-0 6 6
-0 1
-4 0 0
-0 3
-4 5 5
-2 3
-4 1 1
-6 6
-7 7 7
-2 5
-2 6 6
-5 1
-6 0 0
-2 7
-1 6 6
-3 6
-3 7 7
-4 6
-1 7 7
-6 1
-7 0 0
-6 3
-7 2 2
-7 1
-5 3 3
-7 3
-7 4 4
-7 5
-1 3 3
-0 5
-5 7 7
-0 7
-6 4 4
-5 6
-6 7 7
-4 7
-5 0 0
--1 -1
+greedy :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
+           (BoardPosIndex, [BoardPosIndex])
+greedy xs _ = maximumBy best xs
+  where best (a, as) (b, bs) | length as >  length bs = GT
+                             | length as == length bs = EQ
+                             | otherwise              = LT
+
+greedyCorners :: [(BoardPosIndex, [BoardPosIndex])] -> Board ->
+                  (BoardPosIndex, [BoardPosIndex])
+greedyCorners xs _ = maximumBy best xs
+  where best (a, as) (b, bs) | a `elem` corners       = GT
+                             | b `elem` corners       = LT
+                             | a `elem` cornerDiag    = LT
+                             | b `elem` cornerDiag    = GT
+                             | a `elem` cornerEdge    = LT
+                             | b `elem` cornerEdge    = GT
+                             | length as >  length bs = GT
+                             | otherwise              = LT
+                             --no EQ because unnecessary
+
+corners :: [BoardPosIndex]
+corners = map tupleToBoardIndex [(0, 0), (0, 7), (7, 0), (7, 7)]
+
+cornerDiag :: [BoardPosIndex]
+cornerDiag = map tupleToBoardIndex [(1, 1), (1, 6), (6, 1), (6, 6)]
+
+cornerEdge :: [BoardPosIndex]
+cornerEdge = map tupleToBoardIndex [(0, 1), (1, 0), (0, 6), (6, 0), (1, 7),
+                                    (7, 1), (6, 7), (7, 6)]
+
+edges :: [BoardPosIndex]
+edges = map tupleToBoardIndex $ concat $ map
+   (\n -> [(0, n), (7, n), (n, 0), (n, 7)]) [0..7]
 -}
